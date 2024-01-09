04b7541158595707fe0f9ef5f449ee03ab882de4
Maarten Menheere <m.menheere@m2tec.nl>
2024-01-09 21:06:39 +0100

    Fixes

--- 05a_start_cf_ledger_sync_preprod.sh
+++ 05a_start_cf_ledger_sync_preprod.sh
@@ -1,4 +1,6 @@
+#!/bin/bash
+set -x
 export CARDANO_NODE_CONFIG_PATH=/home/maarten/src/lantana/node-config/preprod/config.json
 export NETWORK=preprod
 cd cf-ledger-sync/
-java -jar application/build/libs/ledger-sync-application-0.0.1-f5a9135-SNAPSHOT.jar
+java -jar application/build/libs/ledger-sync-application-0.0.1-3cceb3d-SNAPSHOT.jar
